119a18b92c438035f845c705a8fbd81bb579a194
etiennebacher <you@example.com>
2021-02-17 20:22:09 +0100

    update desc and add argument "files" to unused_functions

--- R/unused_functions.R
+++ R/unused_functions.R
@@ -1,33 +1,45 @@
 #' Find unused functions
 #'
+#' @param files List of files in which to search. If NULL, all .R files are searched in.
+#'
 #' @return Names of unused functions
 #' @export
 #'
+#' @examples
+#' \dontrun{
+#' unused_functions(files = c("some_file.R", "folder/another_file.R"))
+#' }
 
-unused_functions <- function() {
+unused_functions <- function(files = NULL) {
 
+  # Get all function names
   if (is_package()) {
-
-    # Get all function names
     x <- foreman::unpack(path = "R", warn = FALSE)
-    function_names <- purrr::flatten_chr(lapply(x, names))
+  } else {
+    if (is.null(files)) {
+      files <- list.files(pattern = ".R$", recursive = TRUE)
+    } else {
+      files <- files
+    }
 
-    # List functions that are called somewhere else
-    is_called <- lapply(function_names, function(y) {
-      called_by <- foreman::relationship(x, child = y)
-      called_by <- called_by[-(length(called_by)+1)]
-      if (length(called_by) == 0) {
-        return(FALSE)
-      } else {
-        return(TRUE)
-      }
-    })
-
-    # Get functions that are not called
-    functions_not_called <- function_names[which(is_called == FALSE)]
-
-    return(functions_not_called)
-
+    x <- foreman::unpack(path = "R", warn = FALSE)
   }
+  function_names <- purrr::flatten_chr(lapply(x, names))
+
+  # List functions that are called somewhere else
+  is_called <- lapply(function_names, function(y) {
+    called_by <- foreman::relationship(x, child = y)
+    called_by <- called_by[-(length(called_by)+1)]
+    if (length(called_by) == 0) {
+      return(FALSE)
+    } else {
+      return(TRUE)
+    }
+  })
+
+  # Get functions that are not called
+  functions_not_called <- function_names[which(is_called == FALSE)]
+
+  return(functions_not_called)
 
 }
