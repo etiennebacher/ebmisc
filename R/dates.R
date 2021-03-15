0a8b0cdeb89595dcfd4a485f7d3d4c568be31359
etiennebacher <you@example.com>
2021-03-15 23:02:16 +0100

    vectorize month_to_number()

--- R/dates.R
+++ R/dates.R
@@ -1,15 +1,12 @@
 #' Convert month name to month number
 #'
-#' @param month_name Month name (partial match authorized)
+#' @param month_name Month name or vector of month names (partial match authorized)
 #'
 #' @return An integer
 #' @export
 #'
 
 month_to_number <- function(month_name) {
-
-  if (!is.character(month_name))
-    stop("month_name must be the name of a month")
 
   list_conv <- data.frame(
     month_names = c("January", "February", "March", "April", "May",
@@ -18,11 +15,10 @@ month_to_number <- function(month_name) {
     month_number = seq(1, 12, 1)
   )
 
-  return(
-    list_conv[
-      which(grepl(month_name, list_conv$month_names)),
-      2
-    ]
-  )
+  indices <- sapply(month_name, function(x) {
+    which(grepl(x, list_conv$month_names))
+  })
+
+  return(list_conv[indices, 2])
 
 }
